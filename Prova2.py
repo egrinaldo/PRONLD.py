ed5c1717c5b0ce4676e7c023d74b29a2a0b8df91
Junior Cerqueira <egrinaldo19@gmail.com>
2021-11-15 00:04:42 -0300

    LP-A02 Atividade Prática]
    [LP-A02] Crie um algoritmo que recebe como entrada o dia e o mês de nascimento e retorna o signo, seguindo as faixas de valores informados:

---
+++ Prova2.py
@@ -0,0 +1,62 @@
+mes = float(input("Insira o Mês de Nascimento: "))
+dt = int(input("Insira o Dia de Nascimento: "))
+if mes == 1 and dt >= 21 or mes == 2 and dt <= 18:    
+    print("Esse é mesmo o seu signo AQUÁRIO ")    
+    print("Elemento: Ar")    
+    print("Ritmo: Fixo")    
+    print("Cores: Amarelo e Vermelho")
+elif mes == 2 and dt >= 19 or mes == 3 and dt <= 20:   
+    print("Esse é mesmo o seu signo PEIXES")   
+    print("Elemento: Água")    
+    print("Ritmo: Mutável")    
+    print("Cores: Verde e Violeta")
+elif mes == 3 and dt >= 21 or mes == 4 and dt <= 20:    
+    print("Esse é mesmo o seu signo ÁRIES")    
+    print("Elemento: Fogo")    
+    print("Ritmo: Cardinal")    
+    print("Cores: Amarelo, azul e verde")
+elif mes == 4 and dt >= 21 or mes == 5 and dt <= 20:    
+    print("Esse é mesmo o seu signo TOURO")    
+    print("Elemento: Terra")    
+    print("Ritmo: Fixo")    
+    print("Cores: Verde, Violeta e Rosa")
+elif mes == 5 and dt >= 21 or mes == 6 and dt <= 20:    
+    print("Esse é mesmo o seu signo GÊMEOS")    
+    print("Elemento: Ar")    
+    print("Ritmo: Mutável")    
+    print("Cores: Amarela e Verde")
+elif mes == 6 and dt >= 21 or mes == 7 and dt <= 22:    
+    print("Esse é mesmo o seu signo CÂNCER")    
+    print("Elemento: Água")    
+    print("Ritmo: Cardinal")    
+    print("Cores: Rosa e Violeta")
+elif mes == 7 and dt >= 23 or mes == 8 and dt <= 22:    
+    print("Esse é mesmo o seu signo LEÃO")    
+    print("Elemento: Fogo")    
+    print("Ritmo: Fixo")    
+    print("Cores: Amarelo, laranja e verde")
+elif mes == 8 and dt >= 23 or mes == 9 and dt <= 22:    
+    print("Esse é mesmo o seu signo VIRGEM")    
+    print("Elemento: Terra")    
+    print("Ritmo: Mutável")    
+    print("Cores: Azul e Verde")
+elif mes == 9 and dt >= 23 or mes == 10 and dt <= 22:    
+    print("Esse é mesmo o seu signo LIBRA")    
+    print("Elemento: Ar")    
+    print("Elemento: Ar")    
+    print("Cor: Rosa")
+elif mes == 10 and dt >= 23 or mes == 11 and dt <= 21:    
+    print("Esse é mesmo o seu signo ESCORPIÃO")    
+    print("Elemento: Água")    
+    print("Ritmo: Fixo")    
+    print("Cores: Verde, Violeta e Azul Índigo")
+elif mes == 11 and dt >= 22 or mes == 12 and dt <= 21:    
+    print("Esse é mesmo o seu signo SAGITÁRIO")    
+    print("Elemento: Fogo")    
+    print("Ritmo: Mutável")    
+    print("Cores: Laranja, Verde e Violeta")
+elif mes == 12 and dt >= 22 or mes == 1 and dt <= 20:    
+    print("Esse é mesmo o seu signo CAPRICÓRNIO")    
+    print("Elemento: Terra")    
+    print("Ritmo: Cardinal")    
+    print("Cores: Amarelo e Laranja")
