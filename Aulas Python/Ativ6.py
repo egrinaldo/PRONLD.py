c70f4ed91db60fc2cf457c78e33ee688cdfe7481
Junior Cerqueira <egrinaldo19@gmail.com>
2022-01-05 19:03:36 -0300

    Delete Ativ6.py

--- Aulas Python/Ativ6.py
+++
@@ -1,9 +0,0 @@
-def seculo_ano(sec):
-    ano = 0
-    if sec == ano:
-        return sec
-        
-   
-ano = int(input('\nInsira o ano: \n' ))
-ano = ano / (100 + 1)
-print ("\nSeculo: \n" ,round(ano)+1)
